4668c390d832d95453b45b0ade30cc1ab5e7aa22
Aleksandr Popov <popov_a_s@icloud.com>
2025-05-29 22:36:12 +0300

    Add lab 6

--- 2. Разработка фронтальной части web - приложения/Лабораторная 6/main.js
+++ 2. Разработка фронтальной части web - приложения/Лабораторная 6/main.js
@@ -1,1 +1,13 @@
-function ChangeStyle() {}
+
+element = document.getElementById("change");
+
+function ChangeStyle() {
+    element.style.color = "darkgreen";
+    element.style.fontSize = "20px";
+    element.style.textDecoration = "underline";
+    element.style.fontWeight = "bold";
+}
+
+/*ChangeStyle();*/
+
+
