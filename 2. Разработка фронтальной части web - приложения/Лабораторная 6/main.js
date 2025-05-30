aebc5fe59f205829887fd1e8a390174f373b1169
Aleksandr Popov <popov_a_s@icloud.com>
2025-05-30 15:48:44 +0300

    Add project

--- 2. Разработка фронтальной части web - приложения/Лабораторная 6/main.js
+++ 2. Разработка фронтальной части web - приложения/Лабораторная 6/main.js
@@ -1,13 +1,13 @@
-
 element = document.getElementById("change");
 
 function ChangeStyle() {
-    element.style.color = "darkgreen";
-    element.style.fontSize = "20px";
-    element.style.textDecoration = "underline";
-    element.style.fontWeight = "bold";
+  element.style.color = "darkgreen";
+  element.style.fontSize = "20px";
+  element.style.textDecoration = "underline";
+  element.style.fontWeight = "bold";
 }
 
 /*ChangeStyle();*/
 
-
+element.addEventListener("mouseover", ChangeStyle);
+element.addEventListener("mouseout", ChangeStyle);
