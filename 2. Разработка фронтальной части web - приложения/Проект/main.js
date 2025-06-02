a9e643b7f71a44e93f02ff887aa3e3a75c1d4abf
Aleksandr Popov <popov_a_s@icloud.com>
2025-06-02 15:07:17 +0300

    Add for project

--- 2. Разработка фронтальной части web - приложения/Проект/main.js
+++ 2. Разработка фронтальной части web - приложения/Проект/main.js
@@ -1,6 +1,18 @@
 function AddCategory() {
-
   const parentElement = document.getElementById("category");
-  const newCategory = '<div class="cat"><h2>Категория </h2><p>Группа 1</p><p>Группа 2</p><p>Группа 3</p><p>Группа 4</p><p>Группа 5</p></div>';
-  parentElement.insertAdjacentHTML('beforeend', newCategory);
+  const newCategory =
+    '<div class="cat"><h2>Категория </h2><p>Группа 1</p><p>Группа 2</p><p>Группа 3</p><p>Группа 4</p><p>Группа 5</p></div>';
+  parentElement.insertAdjacentHTML("beforeend", newCategory);
+}
+
+let data = [
+  "Александр Попов",
+  "Санкт-Петербург",
+  "popov-a-s@mail.ru",
+  "+7-812-555-55-55",
+];
+
+function AddData() {
+  const html = data.map((item) => `<li>${item}</li>`).join("");
+  document.querySelector("ul").innerHTML = html;
 }
