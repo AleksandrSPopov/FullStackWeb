0a0e38283a508685c04f5c12e075e3c6f54c9684
Aleksandr Popov <popov_a_s@icloud.com>
2025-06-22 16:13:09 +0300

    Add Lab 13

---
+++ 2. Разработка фронтальной части web - приложения/Проект/vite-project/src/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+function Footer() {
+  
+  return (
+    <>
+      <footer>
+        <h3>© 2025 MARKETPLACE</h3>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
---
+++ 2. Разработка фронтальной части web - приложения/Проект/vite-project/src/Header.jsx
@@ -0,0 +1,14 @@
+import './Header.css'
+
+function Header() {
+  
+  return (
+    <>
+      <header>
+        <h1>MARKETPLACE</h1>
+      </header>
+    </>
+  )
+}
+
+export default Header
